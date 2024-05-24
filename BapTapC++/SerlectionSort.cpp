1d9d88a0a133d4cf7ce7e5fc11237fa312c4c039
Nguyễn Thị Ngọc Quyên <124705679+NguyenNgocQuyen29@users.noreply.github.com>
2024-05-24 23:37:52 +0700

    Add files via upload

---
+++ BapTapC++/SerlectionSort.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+
+void sortSelection(int a[], int n) {
+	for (int i = 0; i < n - 1; i++) {
+		int pos_min = i;
+		for (int j = i + 1; j < n; j++) {
+			if (a[j] < a[pos_min]) {
+				pos_min = j;
+			}
+		}
+		swap(a[i], a[pos_min]);
+	}
+}
+
+int main() {
+	int a[1000], n;
+	cout << "Please enter number elements: ";
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+	sortSelection(a, n);
+	for (int i = 0; i < n; i++) {
+		cout << a[i] << " ";
+	}
+	return 0;
+}
